3cde408d7a94191d9f226806d224f34bea98c7b6
Princewill-Awili <princewill.awili@gmail.com>
2022-10-11 11:41:23 +0100

    Implemented USer Summary

--- src/pages/UserDetails/UserDetails.jsx
+++ src/pages/UserDetails/UserDetails.jsx
@@ -1,13 +1,37 @@
 import  './userdetails.css'
 import BackArrow from '../../assets/backArrow.svg'
 import Avatar from '../../assets/avatarUser.svg'
+import Star1 from '../../assets/star1.svg'
+import Star2 from '../../assets/star2.svg'
 
 import Navbar from '../../components/Navbar/Navbar'
 import Sidebar from '../../components/Sidebar/Sidebar'
 
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const UserDetails = () => {
+    const navigate = useNavigate();
+    const [currentSection, setCurrentSection] = useState('general');
+
+    const ratingsNum = Math.floor(Math.random()*4);
+
+    const ratingFunc = (ratingsNum) => {
+        const numUnfilled = Math.floor(3 - ratingsNum);
+        const numFilled = Math.floor(ratingsNum);
+        return [numFilled,numUnfilled];
+      }
+    
+    const [filled,unfilled] = ratingFunc(ratingsNum);
+
+    const moveToSection = (sectionName) =>{
+        setCurrentSection(sectionName);
+        //navigate('/users/:id/${sectionName}')
+
+    }
+
+
+
   return (
     <div className='userdetails'>
         <Navbar/>
@@ -34,10 +58,69 @@ const UserDetails = () => {
                             <img src={Avatar} alt="" className='userImg' />
                             <div className="ustNameArea">
                                 <span className="userName">Grace Effiom</span>
-                                <span className="serialNum">Lg2308BDT</span>
+                                <span className="acctNum">LH2308BDT</span>
                             </div>
                         </div>
+                        <div className="ustCenter">
+                            <span className="userTierTxt">User's Tier</span>
+                            <div className="tierCount">
+                                {[...Array(filled)].map((index) => (
+                                    <img src={Star1} alt="star" key={index}/>
+                                ))}
+                                {[...Array(unfilled)].map((index)=>(
+                                    <img src={Star2} alt="star" key={index} />
+                                ))}
+                            </div>
+
+                        </div>
+                        <div className="ustRight">
+                            <span className="amount">
+                                <span className='amount naira'>&#8358;</span>
+                                200,000.00
+                            </span>
+                            <span className="bankDetails">
+                                9912345678/Providus Bank
+                            </span>
+                        </div>
                     </div>
+                    <ul className="userSummaryBottom">
+                        <li 
+                            className={currentSection === 'general'? "usbSectionIem current" : "usbSectionItem"} 
+                            onClick={()=> moveToSection('general')}
+                        >
+                            General Details
+                        </li>
+                        <li 
+                            className={currentSection === 'documents'? "usbSectionIem current" : "usbSectionItem"} 
+                            onClick={()=> moveToSection('documents')}
+                        >
+                            Documents
+                        </li>
+                        <li 
+                            className={currentSection === 'bankDetails'? "usbSectionIem current" : "usbSectionItem"} 
+                            onClick={()=> moveToSection('bankDetails')}
+                        >
+                            Bank Details
+                        </li>
+                        <li 
+                            className={currentSection === 'loans'? "usbSectionIem current" : "usbSectionItem"} 
+                            onClick={()=> moveToSection('loans')}
+                        >
+                            Loans
+                        </li>
+                        <li 
+                            className={currentSection === 'savings'? "usbSectionIem current" : "usbSectionItem"} 
+                            onClick={()=> moveToSection('savings')}
+                        >
+                            Savings
+                        </li>
+                        <li 
+                            className={currentSection === 'appandsystem'? "usbSectionIem current" : "usbSectionItem"} 
+                            onClick={()=> moveToSection('appandsystem')}
+                        >
+                            App and System
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>  
